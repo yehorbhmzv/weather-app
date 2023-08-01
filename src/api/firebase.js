9565f2f77dbffa0696265d151583e0d7d1b7e14b
yehorbohomazov <ybohomazov@vp.software>
2023-08-02 00:49:01 +0300

    init structure

---
+++ src/api/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDxLqgd9hdYoJW_T8NF1mgEjBbBXev54v0",
+  authDomain: "weather-app-cb9fe.firebaseapp.com",
+  projectId: "weather-app-cb9fe",
+  storageBucket: "weather-app-cb9fe.appspot.com",
+  messagingSenderId: "336040296358",
+  appId: "1:336040296358:web:a8aba0a2a69c469cc09967"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
